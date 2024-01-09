7619aee148e4e7e2a28a1ea362578a756585fa8a
Poojabhatt48 <155936070+Poojabhatt48@users.noreply.github.com>
2024-01-09 11:44:35 +0545

    Add files via upload

---
+++ add.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+int a,b,add;
+printf( "enter a number a");
+scanf("%d",&a);
+
+printf("enter a number b");
+scanf("%d",&b);
+add=a+b;
+   printf("the add is=%d",add); 
+    return 0;
+}
